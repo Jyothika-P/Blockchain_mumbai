c97cbcea24aebfb489aef8fad76ff95a05b58985
DISHA DWIVEDI <dishadwivedi123g@gmai.com>
2023-06-10 15:20:40 +0530

    Pushing all the material I have so far. So far I have only connected the first page.

---
+++ scripts/deploy.js
@@ -0,0 +1,12 @@
+const { ethers } = require('hardhat');
+
+async function main() {
+    const Transaction = await ethers.getContractFactory("transactioncheck");
+    const    contract = await Transaction.deploy();
+    console.log(contract.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+})
