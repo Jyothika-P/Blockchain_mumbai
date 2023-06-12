0da0600684b080f63589e176cce82e1930046ca8
DISHA DWIVEDI <dishadwivedi123g@gmai.com>
2023-06-12 20:20:07 +0530

    full_project

--- contracts/transactioncheck.sol
+++ contracts/transactioncheck.sol
@@ -5,16 +5,19 @@ pragma solidity ^0.8.18;
 
 contract transactioncheck {
     
-    mapping (uint16 => address )  UniqueId;
-    mapping (uint16 => bool)  PresenceCheck;
+    mapping (uint256 => address )  UniqueId;
+    mapping (uint256 => bool)  PresenceCheck;
     
-    function SettingUniqueId(uint16 _UltimateDigitPhoneNumber) public {
-        require(PresenceCheck[_UltimateDigitPhoneNumber] == false);
+    //function to set the uniqueid
+    function SettingUniqueId(uint256 _UltimateDigitPhoneNumber) public {
+       require(PresenceCheck[_UltimateDigitPhoneNumber] == false, "The phone number already exists");
         UniqueId[_UltimateDigitPhoneNumber] = msg.sender;
         PresenceCheck[_UltimateDigitPhoneNumber] = true;
     }
-    function checkAccount(uint16 _UltimateDigitPhoneNumber) public view returns(address){
-        require(PresenceCheck[_UltimateDigitPhoneNumber] == true);
+
+    //function to check the linked account
+    function checkAccount(uint256 _UltimateDigitPhoneNumber) public view returns(address){
+       require(PresenceCheck[_UltimateDigitPhoneNumber] == true, "Phone number not connected to any account");
         return UniqueId[_UltimateDigitPhoneNumber];
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -11,19 +11,27 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 
-// const { API_URL, PRIVATE_KEY } = process.env;
-const API_URL = process.env.API_URL;
+//Exporting the API keys for deployment networks
+const API_URL_MUMBAI = process.env.API_URL_MUMBAI;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const API_URL_MAINNET = process.env.API_URL_MAINNET;
 
+//Deploying network export
 module.exports = {
    solidity: "0.8.18",
+   //Networks used for deployment
    defaultNetwork: "polygon_mumbai",
    networks: {
       hardhat: {},
       polygon_mumbai: {
-         url: API_URL,
+         url: API_URL_MUMBAI,
          accounts: [`0x${PRIVATE_KEY}`],
          chainId: 80001,
-      }
+      },
+      polygon_mainnet : {
+         url : API_URL_MAINNET,
+         accounts : [`0x${PRIVATE_KEY}`],
+         chainId : 137,
+      },
    },
 }
